06e6502ba509ca65e2c712ae94a30134051de48f
Saif Bayyari <saifbayyari@protonmail.com>
2024-08-16 12:30:19 -0400

    we are getting somewhere?

--- main.lua
+++ main.lua
@@ -1,57 +1,39 @@
+-- Require necessary modules
 local Player = require "Player"
-local Scene = require "Scene"
+local SceneManager = require "SceneManager"
 local NPC = require("NPC")
 
+-- Global variable to hold the SceneManager instance
+local sceneManager
+
+-- Initialize the SceneManager and other resources
 function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest')
+    -- Create a SceneManager instance with a name
+    sceneManager = SceneManager:new("Main Scene")
 
-    sti = require 'libraries/sti'
-    gameMap = sti('maps/testMap.lua')
-
-
-
-    local playerImages = {
-        up = loadImages("man/upwards", 11, 21),
-        down = loadImages("man/downwards", 0, 10),
-        left = loadImages("man/sideways", 22, 32),
-        right = loadImages("man/sideways", 22, 32)
-    }
-    local pacmanImages = {
-        up = loadImages("pacman/down", 0, 0),
-        down = loadImages("pacman/down", 0, 0),
-        left = loadImages("pacman/down", 0, 0),
-        right = loadImages("pacman/down", 0, 0)
-    }
-    player = Player:new(400, 300, playerImages, 200, 2)
-    npc = NPC:new(100, 100, pacmanImages, 1000, 0.5)
-    --^this is where we create the pacman, higher speed means more higher shake. speed is currently 1000 as of 8/15/2024
-    npc:setBehavior('follow', player)
-    scene1 = Scene:new("scene1", {player, npc})
-    scene2 = Scene:new("scene2", {})
-
-    game_sequence = {scene1, scene2}
-    current_scene = 1 -- Start with scene1
+    -- Load the initial scene
+    sceneManager:load()
 end
 
+-- Update the current scene and check for input
 function love.update(dt)
-    -- Switch to scene2 when spacebar is pressed
-    if love.keyboard.isDown("lshift") then
-        npc:setBehavior("patrol")
-    elseif love.keyboard.isDown("rshift") then
-        npc:setBehavior("follow", player)
-    end
-
     -- Update the current scene
-    game_sequence[current_scene]:update(dt)
+    sceneManager:update(dt)
+
+    -- Example: Switch to another scene when the spacebar is pressed
+    if love.keyboard.isDown("space") then
+        -- Assuming you have a scene with index 2
+        sceneManager.currentScene = 2
+        sceneManager:load()  -- Load the new scene
+    end
 end
 
+-- Draw the current scene
 function love.draw()
-    gameMap:draw() -- This is how you change the background color of the main screen
-
-    -- Draw the current scene
-    game_sequence[current_scene]:draw()
+    sceneManager:draw()
 end
 
+-- Function to load images from a folder
 function loadImages(folder, startFrame, endFrame)
     local images = {}
 
---
+++ scenemanager.lua
@@ -0,0 +1,127 @@
+-- Define the Scene class
+local Player = require "Player"
+local NPC = require("NPC")
+local sti = require 'libraries/sti'
+
+
+function loadImages(folder, startFrame, endFrame)
+    local images = {}
+
+    for i = startFrame, endFrame do
+        local path = string.format("%s/tile%03d.png", folder, i)
+        if love.filesystem.getInfo(path) then
+            local image = love.graphics.newImage(path)
+            table.insert(images, image)
+        else
+            print("Error: Image not found at path: " .. path)
+        end
+    end
+
+    return images
+end
+
+
+local playerImages = {
+    up = loadImages("man/upwards", 11, 21),
+    down = loadImages("man/downwards", 0, 10),
+    left = loadImages("man/sideways", 22, 32),
+    right = loadImages("man/sideways", 22, 32)
+}
+local pacmanImages = {
+    up = loadImages("pacman/down", 0, 0),
+    down = loadImages("pacman/down", 0, 0),
+    left = loadImages("pacman/down", 0, 0),
+    right = loadImages("pacman/down", 0, 0)
+}
+
+
+
+
+
+-- Define SceneObjects with a scene that contains a game map, player, and NPC
+local SceneObjects = {
+    [1] = (function()
+        local gameMap  -- Declare gameMap as a local variable
+        local npc
+        local player
+
+        -- Load function
+        local function load()
+            love.graphics.setDefaultFilter('nearest', 'nearest')
+            player = Player:new(400, 300, playerImages, 200, 2)
+            npc = NPC:new(100, 100, pacmanImages, 1000, 0.5)
+            npc:setBehavior('follow', player)
+            gameMap = sti('maps/testMap.lua')  -- Initialize gameMap
+        end
+        
+        -- Update function
+        local function update(dt)
+            if gameMap then
+                gameMap:update(dt)
+            end
+
+            player:update(dt)
+            npc:update(dt)
+
+            if love.keyboard.isDown("lshift") then
+                npc:setBehavior("patrol")
+            elseif love.keyboard.isDown("rshift") then
+                npc:setBehavior("follow", player)
+            end
+        end
+        
+        -- Draw function
+        local function draw()
+            if gameMap then
+                gameMap:draw()
+            end
+
+            player:draw()
+            npc:draw()
+        end
+        
+        return {
+            load = load,
+            update = update,
+            draw = draw
+        }
+    end)()
+}
+
+-- Define SceneManager class
+local SceneManager = {}
+SceneManager.__index = SceneManager
+
+-- Constructor
+function SceneManager:new(name, objectList)
+    local self = setmetatable({}, SceneManager)
+    self.name = name or "Default Scene"
+    self.currentScene = 1
+    return self
+end
+
+-- Method to load the current scene
+function SceneManager:load()
+    local scene = SceneObjects[self.currentScene]
+    if scene and scene.load then
+        scene.load()
+    end
+end
+
+-- Method to update the current scene
+function SceneManager:update(dt)
+    local scene = SceneObjects[self.currentScene]
+    if scene and scene.update then
+        scene.update(dt)
+    end
+end
+
+-- Method to draw the current scene
+function SceneManager:draw()
+    local scene = SceneObjects[self.currentScene]
+    if scene and scene.draw then
+        scene.draw()
+    end
+end
+
+return SceneManager
